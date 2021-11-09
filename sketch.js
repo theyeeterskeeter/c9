703cf24e5384328c4c14e185cb765e4eb0590ef4
theyeeterskeeter <93965788+theyeeterskeeter@users.noreply.github.com>
2021-11-09 16:44:46 +1100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,31 @@
+
+function setup() {
+  createCanvas(400,400);
+  box= createSprite (100,240,20,56)
+}
+
+function draw() 
+{
+  background(30);
+  
+  if(keyDown("right")){
+    box.position.x = box.position.x + 5
+  }
+
+  if(keyDown("left")){
+    box.position.x = box.position.x - 5
+  }
+
+  if(keyDown("up")){
+    box.position.y = box.position.y - 5
+  }
+
+  if(keyDown("down")){
+    box.position.y = box.position.y + 5
+  }
+drawSprites()
+}
+
+
+
+
